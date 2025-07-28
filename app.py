702f59d04e39d14e1c5ce902936208ad5857c019
armelgoat <barmel938@gmail.com>
2025-07-28 02:05:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -36,6 +36,6 @@ def convert():
 @app.route('/')
 def home():
     return 'YT-DLP Flask API is running ✅'
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
 
-if __name__ == '__main__':
-    app.run(debug=True)
